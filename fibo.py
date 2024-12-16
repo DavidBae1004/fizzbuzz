1a9b3e69650f636f1f34b88dcfe62d264dbd0207
DavidBae1004 <fahd.bae@gmail.com>
2024-12-16 11:53:01 +0900

    feat: update the fibo code

--- fibo.py
+++ fibo.py
@@ -1,11 +1,16 @@
+
+#def fibo(num):
+#    if num < 2:
+#        return num
+#    else:
+#        return fibo(num-1) + fibo(num-2)
+#print(fibo(5))
+
+from math import sqrt
+
 def fibo(num):
-    if num < 2:
-        return num
-    else:
-        return fibo(num-1) + fibo(num-2)
+    phi = (1 + sqrt(5)) / 2
+  
+    return round(pow(phi, num) / sqrt(5))
 
 print(fibo(5))
-
-
-
-
