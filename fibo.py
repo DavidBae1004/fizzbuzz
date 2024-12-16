1144ed19b9a5ab94bea54026edf1965adfe169e5
DavidBae1004 <fahd.bae@gmail.com>
2024-12-16 12:22:13 +0900

    feat: update the desc

--- fibo.py
+++ fibo.py
@@ -9,8 +9,13 @@
 from math import sqrt
 
 def fibo(num):
+    """
+    fibonacci sequence를 구하는 함수 입니다.
+    """
+  # 점화식 코드
     phi = (1 + sqrt(5)) / 2
   
     return round(pow(phi, num) / sqrt(5))
 
 print(fibo(5))
+
